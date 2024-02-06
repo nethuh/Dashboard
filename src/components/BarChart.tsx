c912fb7d22999f706f364684af0fc1355aa10bb7
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-06 18:12:33 +0530

    data added

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -10,10 +10,64 @@ import {
 
 type Props = {};
 
+const data = [
+    {
+        name: "Jan",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Feb",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Mar",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Apr",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "May",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Jun",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Jul",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Aug",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Sep",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Oct",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Nov",
+        total: Math.floor(Math.random() * 5000) + 1000
+    },
+    {
+        name: "Dec",
+        total: Math.floor(Math.random() * 5000) + 1000
+    }
+];
+
 export default function BarChart({}: Props) {
     return (
         <ResponsiveContainer width={'100%'} height={350}>
-            <BarChart></BarChart>
+            <BarGraph data={data}>
+
+
+            </BarGraph>
         </ResponsiveContainer>
     )
 }
