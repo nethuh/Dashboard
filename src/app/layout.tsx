a82c12a16d83924c20f212807778758aa900e798
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-06 14:56:21 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import SideNavbar from "@/components/SideNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
           }
           )}
       >
-      {/* Sidebar */}
+       <SideNavbar/>
       <p className="border">Sidebar</p>
       {/* main page */}
       <div className="p-8 w-full">
