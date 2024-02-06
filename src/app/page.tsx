b9fdc94af7c90c26d1b90dd7f02bc4c73c3513f1
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-06 17:23:46 +0530

    page tittle done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import PageTitle from "@/components/PageTitle";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className="flex flex-col gap-5 w-full">
+        <PageTitle title="Dashboard"/>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {
+};
+
+export default function Card({}: Props) {
+    return (
+        <></>
+    )
+}
