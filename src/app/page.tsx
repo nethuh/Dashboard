88a270062ba9735db495f9d9b6f61d13fe508a21
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-06 17:43:34 +0530

    card creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,64 @@
 import Image from "next/image";
 import PageTitle from "@/components/PageTitle";
+import {CardProps} from "@/components/Card";
+import {Activity, CreditCard, DollarSign, Users} from "lucide-react";
+import SalesCard, { SalesProps } from "@/components/SalesCard";
+
+const cardData: CardProps[] = [
+    {
+        label: "Total Revenue",
+        amount: "$45,231.89",
+        discription: "+20.1% from last month",
+        icon: DollarSign
+    },
+    {
+        label: "Subscriptions",
+        amount: "+2350",
+        discription: "+180.1% from last month",
+        icon: Users
+    },
+    {
+        label: "Sales",
+        amount: "+12,234",
+        discription: "+19% from last month",
+        icon: CreditCard
+    },
+    {
+        label: "Active Now",
+        amount: "+573",
+        discription: "+201 since last hour",
+        icon: Activity
+    }
+];
+
+const uesrSalesData: SalesProps[] = [
+    {
+        name: "Olivia Martin",
+        email: "olivia.martin@email.com",
+        saleAmount: "+$1,999.00"
+    },
+    {
+        name: "Jackson Lee",
+        email: "isabella.nguyen@email.com",
+        saleAmount: "+$1,999.00"
+    },
+    {
+        name: "Isabella Nguyen",
+        email: "isabella.nguyen@email.com",
+        saleAmount: "+$39.00"
+    },
+    {
+        name: "William Kim",
+        email: "will@email.com",
+        saleAmount: "+$299.00"
+    },
+    {
+        name: "Sofia Davis",
+        email: "sofia.davis@email.com",
+        saleAmount: "+$39.00"
+    }
+];
+
 
 export default function Home() {
   return (
